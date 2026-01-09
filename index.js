df76ea55f0a68da24a7ec5e5b3d62a4ffa81487b
Aglillams <agli.llamas@gmail.com>
2026-01-09 10:59:43 -0600

    Added button functionality
    Wrote js logic to add the point to the scoreboard with the 3 different buttons.

---
+++ index.js
@@ -0,0 +1,34 @@
+let homeScore = 0
+let guestScore = 0
+document.getElementById("home-score").textContent = homeScore
+document.getElementById("guest-score").textContent = guestScore
+
+function add1Home() {
+    homeScore += 1
+    document.getElementById("home-score").textContent = homeScore
+}
+
+function add2Home() {
+    homeScore += 2
+    document.getElementById("home-score").textContent = homeScore
+}
+
+function add3Home() {
+    homeScore += 3
+    document.getElementById("home-score").textContent = homeScore
+}
+
+function add1Guest() {
+    guestScore += 1
+    document.getElementById("guest-score").textContent = guestScore
+}
+
+function add2Guest() {
+    guestScore += 2
+    document.getElementById("guest-score").textContent = guestScore
+}
+
+function add3Guest() {
+    guestScore += 3
+    document.getElementById("guest-score").textContent = guestScore
+}
